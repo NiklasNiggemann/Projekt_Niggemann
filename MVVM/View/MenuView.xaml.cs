ee9607eee6491ffe7cab3e0b2da440666f56bd2f
NiklasNiggemann <96670748+NiklasNiggemann@users.noreply.github.com>
2023-10-21 23:26:58 +0200

    Stable 0.1

--- MVVM/View/MenuView.xaml.cs
+++ MVVM/View/MenuView.xaml.cs
@@ -8,27 +8,10 @@ namespace Mensa_App.Classes.View;
 public partial class MenuView : ContentPage
 {
     MenuViewModel MenuViewModel {  get; set; }
-    public List<object> SelectedMains { get; set; } = new List<object>();
-    public double? SelectedMainsSum { get; set; }
     public MenuView()
 	{
 		InitializeComponent();
         MenuViewModel = new MenuViewModel();
         BindingContext = MenuViewModel;
 	}
-
-    public ICommand SelectedMainsCommand => new Command(() => 
-    {
-        var selectedList = SelectedMains;
-        foreach (var x in SelectedMains)
-        {
-            Dish y = x as Dish;
-            SelectedMainsSum += y.Price;
-        }
-    });
-
-    private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        var element = e.CurrentSelection;
-    }
 }
