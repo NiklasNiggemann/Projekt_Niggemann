260407bcec37f8a96a4d5b878409b4058eaa80b1
NiklasNiggemann <96670748+NiklasNiggemann@users.noreply.github.com>
2023-10-23 12:30:21 +0200

    TimeSpan reworked

--- MVVM/Models/Menu.cs
+++ MVVM/Models/Menu.cs
@@ -11,116 +11,50 @@ namespace Mensa_App.Classes.Models;
 public class Menu
 {
     public string? Date { get; set; }
-    public DateTime UserTimeDateTime { get; set; }
-    private bool IsOpen = false;
-    private bool WillOpen = false;
-    private bool WillClose = false;
-    private bool IsClosedForToday = false;
-    public string UserTimeString
+    public DateTime CurrentTime { get; set; }
+    public string CurrentTimeString
     {
         get
         {
-            return this.UserTimeDateTime.ToString("t") + " Uhr, ";
-        }
-    }
-    public string CheckIfOpen
-    {
-        get
-        {
-            if (UserTimeDateTime.Hour < ClosingTime.Hour)
-            {
-                if (UserTimeDateTime.Hour >= OpeningTime.Hour)
-                {
-                    IsOpen = true;
-                    WillClose = true;
-                    if (UserTimeDateTime.Hour == 11)
-                    {
-                        if (UserTimeDateTime.Minute >= OpeningTime.Minute)
-                        {
-                            WillClose = true;
-                            IsOpen = true;
-                        }
-                        else
-                        {
-                            WillClose = false;
-                            IsOpen = false;
-                            IsClosedForToday = true;
-                        }
-                    }
-                }
-                IsOpen = false;
-                WillOpen = true;
-            }
-            else
-            {
-                IsOpen = false;
-                WillOpen = false;
-                IsClosedForToday = true;
-            }
-
-            if (IsOpen)
-                return " die Mensa ist geöffnet.";
-            else
-                return " die Mensa ist geschlossen.";
-        }
-    }
-    public TimeSpan TimeTillOpen
-    {
-        get
-        {
-            TimeSpan span = new TimeSpan();
-            if (WillOpen)
-            {
-                span = OpeningTime - UserTimeDateTime;
-            }
-            return span;
-        }
-    }
-    public string TimeTillOpenString
-    {
-        get
-        {
-            if (WillOpen)
-                return "Sie öffnet in " + TimeTillOpen.ToFormattedString("t") + "h.";
-            return "";
-        }
-    }
-    public TimeSpan TimeTillClosed
-    {
-        get
-        {
-            TimeSpan span = new TimeSpan();
-            if (WillClose)
-            {
-                span = UserTimeDateTime - ClosingTime;
-            }
-            return span;
-        }
-    }
-    public string TimeTillClosedString
-    {
-        get
-        {
-            if (WillClose)
-                return "Sie schließt in " + TimeTillOpen.ToFormattedString("t") + "h.";
-            else
-                return "Die Mensa öffnet erst morgen wieder."
+            return "Es ist " + CurrentTime.ToString("t") + " Uhr, ";
         }
     }
     public DateTime OpeningTime
     {
         get
         {
-            DateTime dateTime = new DateTime(2023, 10, 23, 11, 30, 0, 0);
-            return dateTime;
+            DateTime openingTime = new DateTime(CurrentTime.Year, CurrentTime.Month, CurrentTime.Day, 11, 30, 0);
+            return openingTime;
         }
     }
     public DateTime ClosingTime
     {
         get
         {
-            DateTime dateTime = new DateTime(2023, 10, 23, 13, 00, 0, 0);
-            return dateTime;
+            DateTime openingTime = new DateTime(CurrentTime.Year, CurrentTime.Month, CurrentTime.Day, 13, 0, 0);
+            return openingTime;
+        }
+    }
+    public string IsOpenString
+    {
+        get
+        {
+            if (CurrentTime.DayOfWeek == DayOfWeek.Saturday || CurrentTime.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return " die Mensa öffnet erst am Montag wieder.";
+            }
+            if (CurrentTime.Hour == 11)
+            {
+                if (CurrentTime.Minute >= 30)
+                {
+                    return " die Mensa ist geöffnet.";
+                }
+            }
+            else if (CurrentTime.Hour > OpeningTime.Hour && CurrentTime.Hour < ClosingTime.Hour)
+            {
+                return " die Mensa ist geöffnet.";
+            }
+            return " die Mensa ist geschlossen.";
         }
     }
     public List<Dish>? MainMenu { get; set; }
@@ -134,8 +68,8 @@ public class Menu
         SoupMenu = new List<Dish>();
         DessertMenu = new List<Dish>();
         HtmlWeb web = new HtmlWeb();
-        UserTimeDateTime = new DateTime();
-        UserTimeDateTime = DateTime.Now;
+        CurrentTime = new DateTime();
+        CurrentTime = DateTime.Now;
         HtmlDocument document = web.Load("https://www.studierendenwerk-pb.de/gastronomie/speiseplaene/mensa-basilica-hamm/");
         GenerateIndividualMenus(document, ".main-dishes");
         GenerateIndividualMenus(document, ".side-dishes");
