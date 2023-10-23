156a141e820fc3b8e863c519ac32152eb59b0c27
NiklasNiggemann <96670748+NiklasNiggemann@users.noreply.github.com>
2023-10-23 03:53:30 +0200

    simple first time implementation

--- MVVM/Models/Menu.cs
+++ MVVM/Models/Menu.cs
@@ -10,6 +10,52 @@ namespace Mensa_App.Classes.Models;
 public class Menu
 {
     public string? Date { get; set; }
+    public DateTime UserTimeDateTime { get; set; }
+    public string UserTimeString
+    {
+        get
+        {
+            return this.UserTimeDateTime.ToString("t") + " Uhr, ";
+        }
+    }
+    public string CheckIfOpen
+    {
+        get
+        {
+            if (UserTimeDateTime.Hour < ClosingTime.Hour)
+            {
+                if (UserTimeDateTime.Hour > OpeningTime.Hour)
+                {
+                    if (UserTimeDateTime.Hour == 11)
+                    {
+                        if (UserTimeDateTime.Minute >= OpeningTime.Minute)
+                            return " die Mensa ist geöffnet.";
+                        else
+                            return " die Mensa ist geschlossen.";
+                    }
+                    return " die Mensa ist geöffnet.";
+                }
+                return " die Mensa ist geschlossen.";
+            }
+            return " die Mensa ist geschlossen.";
+        }
+    }
+    public DateTime OpeningTime
+    {
+        get
+        {
+            DateTime dateTime = new DateTime(2000, 01, 01, 11, 30, 0, 0);
+            return dateTime;
+        }
+    }
+    public DateTime ClosingTime
+    {
+        get
+        {
+            DateTime dateTime = new DateTime(2000, 01, 01, 13, 00, 0, 0);
+            return dateTime;
+        }
+    }
     public List<Dish>? MainMenu { get; set; }
     public List<Dish>? SideMenu { get; set; }
     public List<Dish>? SoupMenu { get; set; }
@@ -21,6 +67,8 @@ public class Menu
         SoupMenu = new List<Dish>();
         DessertMenu = new List<Dish>();
         HtmlWeb web = new HtmlWeb();
+        UserTimeDateTime = new DateTime();
+        UserTimeDateTime = DateTime.Now;
         HtmlDocument document = web.Load("https://www.studierendenwerk-pb.de/gastronomie/speiseplaene/mensa-basilica-hamm/");
         GenerateIndividualMenus(document, ".main-dishes");
         GenerateIndividualMenus(document, ".side-dishes");
