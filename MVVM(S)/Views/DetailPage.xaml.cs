736866f0e79ac26cb8559512040bd10f437378ad
NiklasNiggemann <96670748+NiklasNiggemann@users.noreply.github.com>
2023-11-30 16:53:27 +0100

    finalized

---
+++ MVVM(S)/Views/DetailPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Mensa_App.MVVMS.Views;
+
+public partial class DetailPage : TabbedPage
+{
+	public DetailPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MVVM(S)/Views/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Mensa_App.MVVMS.ViewModels;
+using Mensa_App.MVVMS.Views;
+using Mensa_App.MVVMS.Models;
+
+namespace Mensa_App;
+
+public partial class MainPage : FlyoutPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
